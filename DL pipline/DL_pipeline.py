84923de39b78d78b89109631fb30dd02d5a80716
Akhil Ratan Kattyayan <Akhilratanm@gmail.com>
2021-11-08 13:03:20 +0530

    Create DL_pipeline.py

---
+++ DL pipline/DL_pipeline.py
@@ -0,0 +1,254 @@
+import keras
+from keras import backend as K
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping, TensorBoard
+from keras import losses
+from sklearn.model_selection import train_test_split
+
+# Custom loss SSD
+def ssd_loss(y_true, y_pred):
+    return K.sum(K.square(y_pred - y_true), axis=-2)
+
+# Combined loss SSD + MSE
+def combined_ssd_mse_loss(y_true, y_pred):
+    return K.mean(K.square(y_true - y_pred), axis=-2) * 500 + K.sum(K.square(y_true - y_pred), axis=-2)
+
+def combined_ssd_mad_loss(y_true, y_pred):
+    return K.max(K.square(y_true - y_pred), axis=-2) * 50 + K.sum(K.square(y_true - y_pred), axis=-2)
+
+# Custom loss SAD
+def sad_loss(y_true, y_pred):
+    return K.sum(K.sqrt(K.square(y_pred - y_true)), axis=-2)
+
+# Custom loss MAD
+def mad_loss(y_true, y_pred):
+    return K.max(K.square(y_pred - y_true), axis=-2)
+
+
+def train_dl(Dataset, experiment):
+
+    print('Deep Learning pipeline: Training the model for exp ' + str(experiment))
+
+    [X_train, y_train, X_test, y_test] = Dataset
+
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.3, shuffle=True, random_state=1)
+
+    # ==================
+    # LOAD THE DL MODEL
+    # ==================
+
+
+    if experiment == 'FCN-DAE':
+        # FCN_DAE
+        model = FCN_DAE()
+        model_label = 'FCN_DAE'
+
+    if experiment == 'DRNN':
+        # DRNN
+        model = DRRN_denoising()
+        model_label = 'DRNN'
+
+    if experiment == 'Vanilla L':
+        # Vanilla CNN linear
+        model = deep_filter_vanilla_linear()
+        model_label = 'Vanilla_L'
+
+    if experiment == 'Vanilla NL':
+        # Vanilla CNN non linear
+        model = deep_filter_vanilla_Nlinear()
+        model_label = 'Vanilla_NL'    
+
+    if experiment == 'Multibranch LANL':
+        # Multibranch linear and non linear
+        model = deep_filter_I_LANL()
+        model_label = 'Multibranch_LANL'
+
+    if experiment == 'Multibranch LANLD':
+        # Inception-like linear and non linear dilated
+        model = deep_filter_model_I_LANL_dilated()
+        model_label = 'Multibranch_LANLD'
+
+    if experiment == 'LSTM IEEE':
+        # LSTM IEEE
+        model = LSTM_denoising()
+        model_label = 'LSTM_IEEE'
+
+    if experiment == 'CNN IEEE':
+        # CNN IEEE
+        model = CNN_Denoising()
+        model_label = 'CNN_IEEE'
+
+    if experiment == 'CNN autoencoder':
+        # CNN encoder decoder
+        model = CNN_encoder_decoder()
+        model_label = 'CNN_autoencoder'
+
+
+    print('\n ' + model_label + '\n ')
+
+    model.summary()
+
+    epochs = int(1e3)
+    #epochs = int(1e5)  # 100000
+    # epochs = 100
+    batch_size = 128
+    lr = 1e-1
+    #lr = 1e-3
+    # lr = 1e-4
+    minimum_lr = 1e-3
+    #minimum_lr = 1e-10
+
+
+    # Loss function selection according to method implementation
+    if experiment == 'DRNN' or experiment == 'LSTM IEEE' or experiment == 'CNN IEEE':
+        criterion = keras.losses.mean_squared_error
+
+    elif experiment == 'FCN-DAE' or experiment == 'CNN autoencoder':
+        criterion = ssd_loss
+
+    else:
+        criterion = combined_ssd_mad_loss
+
+
+    model.compile(loss=criterion,
+                  optimizer=keras.optimizers.Adam(lr=lr),
+                  metrics=[losses.mean_squared_error, losses.mean_absolute_error, ssd_loss, mad_loss])
+
+    # Keras Callbacks
+
+    # checkpoint
+    model_filepath = model_label + '_weights.best.hdf5'
+
+    checkpoint = ModelCheckpoint(model_filepath,
+                                 monitor="val_loss",
+                                 verbose=1,
+                                 save_best_only=True,
+                                 mode='min',  # on acc has to go max
+                                 save_weights_only=True)
+
+    reduce_lr = ReduceLROnPlateau(monitor="val_loss",
+                                  factor=0.5,
+                                  min_delta=0.05,
+                                  mode='min',  # on acc has to go max
+                                  patience=2,
+                                  min_lr=minimum_lr,
+                                  verbose=1)
+
+    early_stop = EarlyStopping(monitor="val_loss",  # "val_loss"
+                               min_delta=0.05,
+                               mode='min',  # on acc has to go max
+                               patience=10,
+                               verbose=1)
+
+    tb_log_dir = './runs/' + model_label
+
+    tboard = TensorBoard(log_dir=tb_log_dir, histogram_freq=0,
+                         write_graph=False, write_grads=False,
+                         write_images=False, embeddings_freq=0,
+                         embeddings_layer_names=None,
+                         embeddings_metadata=None)
+
+    # To run the tensor board
+    # tensorboard --logdir=./runs
+
+    # GPU
+    model.fit(x=X_train, y=y_train,
+              validation_data=(X_val, y_val),
+              batch_size=batch_size,
+              epochs=epochs,
+              verbose=1,
+              callbacks=[early_stop,
+                         reduce_lr,
+                         checkpoint,
+                         tboard])
+
+    K.clear_session()
+
+
+
+def test_dl(Dataset, experiment):
+
+    print('Deep Learning pipeline: Testing the model')
+
+    [train_set, train_set_GT, X_test, y_test] = Dataset
+
+    batch_size = 32
+
+    # ==================
+    # LOAD THE DL MODEL
+    # ==================
+
+    if experiment == 'FCN-DAE':
+        # FCN_DAE
+        model = FCN_DAE()
+        model_label = 'FCN_DAE'
+
+    if experiment == 'DRNN':
+        # DRNN
+        model = DRRN_denoising()
+        model_label = 'DRNN'
+
+    if experiment == 'Vanilla L':
+        # Vanilla CNN linear
+        model = deep_filter_vanilla_linear()
+        model_label = 'Vanilla_L'
+
+    if experiment == 'Vanilla NL':
+        # Vanilla CNN non linear
+        model = deep_filter_vanilla_Nlinear()
+        model_label = 'Vanilla_NL'    
+
+    if experiment == 'Multibranch LANL':
+        # Multibranch linear and non linear
+        model = deep_filter_I_LANL()
+        model_label = 'Multibranch_LANL'
+
+    if experiment == 'Multibranch LANLD':
+        # Inception-like linear and non linear dilated
+        model = deep_filter_model_I_LANL_dilated()
+        model_label = 'Multibranch_LANLD'
+
+    if experiment == 'LSTM IEEE':
+        # LSTM IEEE
+        model = LSTM_denoising()
+        model_label = 'LSTM_IEEE'
+
+    if experiment == 'CNN IEEE':
+        # CNN IEEE
+        model = CNN_Denoising()
+        model_label = 'CNN_IEEE'
+
+    if experiment == 'CNN autoencoder':
+        # CNN encoder decoder
+        model = CNN_encoder_decoder()
+        model_label = 'CNN_autoencoder'
+
+    print('\n ' + model_label + '\n ')
+
+    model.summary()
+
+    # Loss function selection according to method implementation
+    if experiment == 'DRNN' or experiment == 'LSTM IEEE' or experiment == 'CNN IEEE':
+        criterion = 'mse'
+
+    elif experiment == 'FCN-DAE' or experiment == 'CNN autoencoder':
+        criterion = ssd_loss
+
+    else:
+        criterion = combined_ssd_mad_loss
+
+    model.compile(loss=criterion,
+                  optimizer=keras.optimizers.Adam(lr=0.01),
+                  metrics=[losses.mean_squared_error, losses.mean_absolute_error, ssd_loss, mad_loss])
+
+    # checkpoint
+    model_filepath = model_label + '_weights.best.hdf5'
+    # load weights
+    model.load_weights(model_filepath)
+
+    # Test score
+    y_pred = model.predict(X_test, batch_size=batch_size, verbose=1)
+
+
+    K.clear_session()
+
+    return [X_test, y_test, y_pred]
