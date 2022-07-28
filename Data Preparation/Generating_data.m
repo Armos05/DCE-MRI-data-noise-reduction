25e2d32b0825fa1d50625b50dc4b9d1dc7b31baf
Akhil Ratan Mishra <Akhilratanm@gmail.com>
2022-07-28 16:58:17 +0530

    Update Generating_data.m

--- Data Preparation/Generating_data.m
+++ Data Preparation/Generating_data.m
@@ -75,3 +75,95 @@ writematrix(n_BigtrainXn,'TrainXn.csv');
 writematrix(n_BigtrainY,'TrainY.csv');
 writematrix(n_BigtestXn,'TestXn.csv');
 writematrix(n_BigtestY,'TestY.csv');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% EOF%%%%%%%%%%%%%%%%%%%%%%%%%%
+for i = 1:50
+	for j = 1:50
+		for k = 1:50
+		     BigX(:, q) = ctfun_ori_RD([0.01 + 0.006*k , 0.1 + 0.04*j, 0.001 + 0.006*i],T);
+			 noise_level = noise_list(randi(length(noise_list)));
+			 noise = maxConc*noise_level*randn(length(T),1); % random noise with normal distribution (u=0, sd=1)  
+			 BigNX(:,q) = BigX(:,q) + noise;
+			 q = q+1;
+		end
+		
+	end
+end
+
+plot(BigX(:,1)), hold on;
+plot(BigX(:,34)), hold on;
+plot(BigX(:,17)), hold on;
+plot(BigNX(:,1)), hold on;
+plot(BigNX(:,34)), hold on;
+plot(BigNX(:,17));
+legend('Denoised Data', 'Noised Values');
+title('Comparison between noised and denoised data')
+xlabel('Time intervals')
+ylabel('Concentration value (C_t)');
+
+% how to shuffle
+
+BigNX_train = BigNX(:,1:100000);
+BigNX_test = BigNX(:,100001:125000);
+Bigy_train = BigX(:,1:100000);
+Bigy_test = BigX(:,100001:125000);
+
+
+plot(BigNX_test(:,131)), hold on;
+plot(Bigy_test(:,131)), hold on;
+plot(BigY_pred_poland(:,131));
+legend('Denoised Data', 'Noised Values', 'Predicted Denoised');
+title('Comparison between noised and denoised data')
+xlabel('Time intervals')
+ylabel('Concentration value (C_t)')
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+sum(sqrt(mean((Y_train-X_train).^2)))
+ans = 221.6130
+
+sum(sqrt(mean((Y_test-X_test).^2)))
+ans = 108.6578
+
+X_train_f = X_train(:,1:17825);
+X_test_f = X_test(:,1:8775);
+Y_train_f = Y_train(:,1:17825);
+Y_test_f = Y_test(:,1:8775);
+
+X_train_fe = zeros(800,713);
+X_test_fe = zeros(800,351);
+Y_train_fe = zeros(800,713);
+Y_test_fe = zeros(800,351);
+
+
+for i = 1:713
+	for j = 1:25
+		X_train_fe(32*(j-1)+1:32*j,i) = X_train_f(:,j);
+		Y_train_fe(32*(j-1)+1:32*j,i) = Y_train_f(:,j); 
+	end
+end	
+
+for i = 1:351
+	for j = 1:25
+		X_test_fe(32*(j-1)+1:32*j,i) = X_test_f(:,j);
+		Y_test_fe(32*(j-1)+1:32*j,i) = Y_test_f(:,j); 
+	end
+end	
+
+X_train_fee = zeros(800,713);
+X_test_fee = zeros(800,315);
+
+for i = 1:713
+	noise_level = noise_list(randi(length(noise_list)));
+    noise = noise_level*randn(800,1); % random noise with normal distribution (u=0, sd=1)  
+    X_train_fee(:, i) = X_train_fe(:,i) + noise;
+end
+
+for i = 1:351
+	noise_level = noise_list(randi(length(noise_list)));
+    noise = noise_level*randn(800,1); % random noise with normal distribution (u=0, sd=1)  
+    X_test_fee(:, i) = X_test_fe(:,i) + noise;
+end
+
+%
+sum(sqrt(mean((Y_train_fe-X_train_fee).^2)))
+
